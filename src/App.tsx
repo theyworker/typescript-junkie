9123b8a415b6ac62610e15baa7b54e4de6005d92
Devaka <devaka.kumarasinghe@gmail.com>
2022-04-14 00:37:23 +0530

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-
 import './App.css';
 
 const App  = () => {
-  let firstValue: string = 'Manny'
+  let firstValue: [string, number]  = ['hi', 2]
+
+  enum Codes  {first = 1, second = 2}
+
+  let secondValue : Codes = Codes.first
+
+  const warning =(): void => {
+    console.log('warning');
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-        The value {firstValue} is of {typeof firstValue} type!
+        The value {secondValue} is of {typeof secondValue} type!
         </p>
-       
       </header>
     </div>
   );
